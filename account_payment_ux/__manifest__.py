590558961b02ee29cc1246de7608989fa2458eac
ntsystemwork <40502705+ntsystemwork@users.noreply.github.com>
2024-04-19 15:59:15 -0300

    Update __manifest__.py
    upd 17

--- account_payment_ux/__manifest__.py
+++ account_payment_ux/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment UX",
-    "version": "16.0.1.0.0",
+    "version": "17.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
